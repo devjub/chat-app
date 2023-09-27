e73c62c57c7071775d0e0c519afcc3bdcadfe5e0
Edikan Nkanta <edikan.nkanta@lawrencepriceltd.com>
2023-09-27 23:03:43 +0100

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
  
  
  function show() {
+   
     var txt = document.getElementById("text").value;
     var i = document.getElementById("screen").innerHTML = txt;
-    i++;
+    
+    }
+
+    function back() {
+      document.getElementById("home").style.display = "flex";
+      document.getElementById("chatapp").style.display = "none";
+      
+   }
+
+ function sho() {
+   document.getElementById("home").style.display = "none";
+   document.getElementById("chatapp").style.display = "flex";
+
+}
+ 
+ function tune() {
+  document.getElementById("home").style.display = "none";
+
+  document.getElementById("call").style.display = "flex";
+  
  }
+ 
